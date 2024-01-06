83748d64f100a998b19cc92428a732d59d1eecd3
JosephNguyenNazareth <iohkgn9861@gmail.com>
2024-01-06 23:07:26 +0100

    last commit before changing to version 2

--- src/main/java/com/pmsconnect/mage/ontology/Ontology.java
+++ src/main/java/com/pmsconnect/mage/ontology/Ontology.java
@@ -1,84 +1,73 @@
 package com.pmsconnect.mage.ontology;
 
-import org.semanticweb.HermiT.ReasonerFactory;
-import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.*;
-import org.semanticweb.owlapi.reasoner.OWLReasoner;
-import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+import static org.semanticweb.owlapi.search.Searcher.annotations;
+import static org.semanticweb.owlapi.vocab.OWLRDFVocabulary.RDFS_LABEL;
+import static org.semanticweb.owlapi.vocab.OWLRDFVocabulary.RDFS_SUBCLASS_OF;
 
-import javax.annotation.Nonnull;
 import java.io.File;
-import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.semanticweb.owlapi.search.EntitySearcher.getAnnotationObjects;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLAnnotationProperty;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLException;
+import org.semanticweb.owlapi.model.OWLLiteral;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 public class Ontology {
-    @Nonnull
-    private final ReasonerFactory reasonerFactory;
-    @Nonnull
-    private final OWLOntology ontology;
-    private final PrintStream out;
-    private Ontology(@Nonnull OWLOntology inputOntology) {
-        this.reasonerFactory = new ReasonerFactory();
-        ontology = inputOntology;
-        out = System.out;
+    protected final OWLOntology ontology;
+    private OWLDataFactory df;
+
+    public Ontology(OWLOntology ontology, OWLDataFactory df) {
+        this.ontology = ontology;
+        this.df = df;
     }
 
-    private void printHierarchy(@Nonnull OWLClass clazz) {
-        OWLReasoner reasoner = reasonerFactory.createReasoner(ontology);
-        printHierarchy(reasoner, clazz, 0);
-        /* Now print out any unsatisfiable classes */
-        for (OWLClass cl : ontology.getClassesInSignature()) {
-            assert cl != null;
-            if (!reasoner.isSatisfiable(cl)) {
-                out.println("XXX: " + labelFor(cl));
-            }
-        }
-        reasoner.dispose();
-    }
-
-    private String labelFor(@Nonnull OWLClass clazz) {
-        /*
-         * Use a visitor to extract label annotations
-         */
-        LabelExtractor le = new LabelExtractor();
-        for (OWLAnnotation anno : getAnnotationObjects(clazz, ontology)) {
-            anno.accept(le);
-        }
-        /* Print out the label if there is one. If not, just use the class URI */
-        if (le.getResult() != null) {
-            return le.getResult();
-        } else {
-            return clazz.getIRI().toString();
+    public void parseOntology() {
+        for (OWLClass cls : ontology.getClassesInSignature()) {
+            String id = cls.getIRI().toString();
+            String label = get(cls, RDFS_LABEL.toString()).get(0);
+            System.out.println(label + " [" + id + "]");
         }
     }
 
-    private void printHierarchy(@Nonnull OWLReasoner reasoner, @Nonnull OWLClass clazz, int level) {
-        /*
-         * Only print satisfiable classes -- otherwise we end up with bottom everywhere
-         */
-        if (reasoner.isSatisfiable(clazz)) {
-            for (int i = 0; i < level * 4; i++) {
-                out.print(" ");
-            }
-            out.println(labelFor(clazz));
-            /* Find the children and recurse */
-            for (OWLClass child : reasoner.getSubClasses(clazz, true).getFlattened()) {
-                if (!child.equals(clazz)) {
-                    printHierarchy(reasoner, child, level + 1);
+    public void parseProperties() {
+        for (OWLClass cls : ontology.getClassesInSignature()) {
+            String id = cls.getIRI().toString();
+            String label = get(cls, RDFS_SUBCLASS_OF.toString()).get(0);
+            System.out.println(label + " [" + id + "]");
+        }
+    }
+
+    private List<String> get(OWLClass clazz, String property) {
+        List<String> ret = new ArrayList<>();
+
+        final OWLAnnotationProperty owlProperty = df
+                .getOWLAnnotationProperty(IRI.create(property));
+        for (OWLOntology o : ontology.getImportsClosure()) {
+            for (OWLAnnotation annotation : annotations(
+                    o.getAnnotationAssertionAxioms(clazz.getIRI()), owlProperty)) {
+                if (annotation.getValue() instanceof OWLLiteral) {
+                    OWLLiteral val = (OWLLiteral) annotation.getValue();
+                    ret.add(val.getLiteral());
                 }
             }
         }
+        return ret;
     }
 
-    public static void main(String[] args) throws OWLOntologyCreationException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws OWLException {
+        File file = new File("/home/nguyenminhkhoi/Documents/doctorat/etude/article/BPM2023/pmage.owx");
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-        File ontologyFile = new File("/home/nguyenminhkhoi/Documents/doctorat/etude/article/BPM2023/pmage.owx");
-        String reasonerFactoryClassName = null;
-        OWLOntology ontology = manager.loadOntologyFromOntologyDocument(ontologyFile);
-        Ontology simpleHierarchy = new Ontology(ontology);
-        OWLClass clazz = manager.getOWLDataFactory().getOWLThing();
-        System.out.println("Class       : " + clazz);
-        simpleHierarchy.printHierarchy(clazz);
+        OWLOntology ontology = manager.loadOntologyFromOntologyDocument(file);
+        Ontology parser = new Ontology(ontology, manager.getOWLDataFactory());
+
+        ontology.getAnnotationPropertiesInSignature();
+        parser.parseProperties();
     }
 }
